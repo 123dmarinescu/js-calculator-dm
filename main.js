0bd76cc0bd5d7714f1cf356b93db84a2c234f718
Dan Marinescu <dan.marinescu@codefactorygroup.com>
2022-06-16 06:50:02 +0300

    added displayResult

--- main.js
+++ main.js
@@ -6,7 +6,18 @@
 
 const buttons = document.querySelector('.buttons')
 
-buttons.addEventListener('click', function(e){
-    console.log('text112')
-})
-console.log(buttons)
+// buttons.addEventListener('click', function(e){
+//     var text = buttons.innerText
+//     console.log(text)
+//     var displayResult = document.getElementsByClassName('display-result').innerText = text
+//     displayResult.innerText = text
+// })
+
+const MC = document.getElementById('MC')
+
+const a = MC.addEventListener('click', function(e){
+    let text = MC.innerText
+    let displayResult = document.querySelector('h1')
+    displayResult.innerHTML = text
+    })
+
